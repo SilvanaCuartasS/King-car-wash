25d72c50b77e514057e76f75201f737ed55d1940
SilvanaCuartasS <silvanacuartassilva@gmail.com>
2025-04-30 14:50:09 -0500

    cambios

--- app_user/app.js
+++ app_user/app.js
@@ -1,3 +1,5 @@
+import renderScreenUserLogin1 from "./screens/screenLoginUser.js";
+import renderScreenUserSignUp1 from "./screens/screenSignUpUser1.js";
 import renderScreenUser1 from "./screens/screenUser1.js";
 
 const socket = io("/", { path: "/real-time" });
@@ -7,29 +9,52 @@ function clearScripts() {
 }
 
 let route = { path: "/", data: {} };
+router();
 
-switch (route.path) {
-  case "/":
-    clearScripts();
-    renderScreenUser1(route.data);
-    break;
-  default:
-    const app = document.getElementById("app");
-    app.innerHTML = `<h1>404 - Not Found</h1><p>The page you are looking for does not exist.</p>`;
+function router() {
+  switch (route.path) {
+    case "/":
+      // clearScripts();
+      renderScreenUser1(route);
+      break;
+
+    case "/loginUser1":
+      // clearScripts();
+      renderScreenUserLogin1();
+      break;
+    
+    case "/signUpUser1":
+      // clearScripts();
+      renderScreenUserSignUp1();
+      break;
+
+    default:
+      const app = document.getElementById("app");
+      app.innerHTML = `<h1>404 - Not Found</h1><p>The page you are looking for does not exist.</p>`;
+  }
 }
 
 function navigateTo(path, data) {
+  console.log(path); //aqui si llega
   route = { path, data };
+  router();
+  console.log(route);
 }
 
 async function makeRequest(url, method, body) {
+
+  //bien
+  console.log(url);
+  console.log(method);
+  console.log(body);
+  
   const BASE_URL = "http://localhost:5057";
   let response = await fetch(`${BASE_URL}${url}`, {
     method: method,
     headers: {
       "Content-Type": "application/json",
     },
-    body: body ? JSON.stringify(body): null,
+    body: body ? JSON.stringify(body) : null,
   });
 
   response = await response.json();
--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -6,6 +6,36 @@ const getUsers = async (req, res) => {
   res.send(users);
 };
 
+const loginService = async (req, res) => {
+  
+  const users = await getAllUsers();
+  // comparar si el usuario que se logueo existe en la BD
+  const aver = { inputEmail, inputPassword } = req.body;
+
+  if (!inputEmail || !inputPassword) {
+    return res
+      .status(400)
+      .json({ message: "Ops, faltan datos", success: false });
+  }
+
+  const foundUser = users.find(
+    (u) => u.email === inputEmail && u.password === inputPassword
+  );
+
+  if (!foundUser) {
+    return res
+      .status(401)
+      .json({
+        message: "Usuario no existe o contraseña incorrecta",
+        success: false,
+      });
+  }
+
+  console.log("Inicio de sesión exitoso:", foundUser);
+  res.json({ message: "Inicio de sesión exitoso", success: true });
+  res.send(users); //Envio al cliente lo que tenga users
+};
+
 const createUser = async (req, res) => {
   const { id, name } = req.body;
   const response = await createUser({ id, name });
@@ -14,5 +44,6 @@ const createUser = async (req, res) => {
 
 module.exports = {
   getUsers,
+  loginService,
   createUser,
 };
