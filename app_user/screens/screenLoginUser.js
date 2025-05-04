ca695a0619434fe705142f5b468d6633c758068a
Juancastrog10 <juancastrogrimaldi10@gmail.com>
2025-05-04 16:33:59 -0500

    css log in cliente

--- app_user/screens/screenLoginUser.js
+++ app_user/screens/screenLoginUser.js
@@ -1,35 +1,57 @@
 import { makeRequest, navigateTo } from "../app.js";
 
+function loadCSS(href) {
+  const existingLink = document.querySelector(`link[href="${href}"]`);
+  if (!existingLink) {
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = href;
+    document.head.appendChild(link);
+  }
+}
+
 export default function renderScreenUserLogin1() {
   const app = document.getElementById("app");
-  app.innerHTML = `
-    
-    <div id="logo-container">
-    <img src="" alt="Logo" id="logo">
-    </div>
 
-    <div id="login-container">
+    // 👇 Carga el CSS al renderizar esta pantalla
+    loadCSS("/app_user/styles/screenLoginUser.css");
+
+    app.innerHTML = `
+<div id="login-wrapper">
+  <div id="logo-container">
+    <img src="/app_user/assets/logo-king.png" alt="Logo" id="logo">
+  </div>
+
+  <div id="login-container">
     <button id="backBTN">Back</button>
     <h1>LOG IN</h1>
-    <img src="" alt="userIcon" id="userIcon">
-    <input type="email" id="email" placeholder="email">
-    <img src="" alt="lockIcon" id="lockIcon">
+    <p>Welcome back!</p>
 
-      <div class="password-wrapper">
-        <input type="password" id="password" placeholder="password">
-        <button type="button" id="togglePassword">Show</button>
-      </div>
-    <p>Forgot password? Remember in HERE</p>
+    <div class="input-group">
+      <img src="/app_user/assets/user-icon.png" alt="userIcon">
+      <input type="email" id="email" placeholder="Email">
+    </div>
 
+    <div class="input-group password-wrapper">
+      <img src="/app_user/assets/lock-icon.png" alt="lockIcon">
+      <input type="password" id="password" placeholder="Password">
+      <button type="button" id="togglePassword">Show</button>
+    </div>
+
+    <p>Forgot password? Remember in <a href="#">HERE</a></p>
     <button id="log-in">Log in</button>
     <p>Or sign in with</p>
-    <img src="" alt="FacebookIcon" id="FacebookIcon">
-    <img src="" alt="igIcon" id="igIcon">
-    
-    <a href="#" id="sign-up" >Haven't registered, What are you waiting for?  Sign up</a>
+    <div class="social-icons">
+      <img src="/app_user/assets/facebook-icon.png" id="FacebookIcon" alt="Facebook">
+      <img src="/app_user/assets/instagram-icon.png" id="igIcon" alt="Instagram">
     </div>
-      
-      `;
+    <div id="sign-up-text">
+  <span>Haven’t registered, What are you waiting for?</span>
+  <a href="#" id="sign-up">Sign up</a>
+</div>
+  </div>
+</div>
+`;
 
   const inputEmail = document.getElementById("email");
   const inputPassword = document.getElementById("password");
