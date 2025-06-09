b382e9705c203ceddfbc028d0b2361114f44de02
Majorosas16 <mariajose.rosascuellar@gmail.com>
2025-06-09 08:55:19 -0500

    supabaseTry

--- server/db/admins.db.js
+++ server/db/admins.db.js
@@ -1,38 +1,19 @@
-// Solo obtener y visualizar la DB
-let admins = [
-  {
-    id: 1,
-    name: "David",
-    adminCode: "A4123",
-    email: "davidAdmin@gmail.com",
-    password:"13579",
-  },
-  {
-    id: 2,
-    name: "Luis",
-    adminCode: "A5823",
-    email: "luisAdmin@gmail.com",
-    password:"24680",
-  },
- { 
-    id: 3,
-    name: "Juan",
-    adminCode: "A6089",
-    email: "juanAdmin@gmail.com",
-    password:"654321",
- },
- {
-  id: 4,
-  name: "David",
-  adminCode: "1",
-  email: "d",
-  password:"1",
-},
-];
+const supabaseCli = require("../services/supabase.service");
+
+// const getAllAdmins = async () => {
+//   return admins;
+// };
 
 const getAllAdmins = async () => {
-  return admins;
+  const { data, error } = await supabaseCli.from("Admin").select();
+  if (error) {
+    console.error(error);
+    return error;
+  }
+  return data;
 };
+
+
 
 module.exports = {
   getAllAdmins,
--- server/db/users.db.js
+++ server/db/users.db.js
@@ -1,14 +1,36 @@
-// Solo obtener y visualizar la DB
-let users = [];
+const supabaseCli = require("../services/supabase.service");
+
+// const getAllUsers = async () => {
+//   return users;
+// };
 
 const getAllUsers = async () => {
-  return users;
+  const { data, error } = await supabaseCli.from("Usuario").select();
+  if (error) {
+    console.error(error);
+    return error;
+  }
+  return data;
 };
 
 const createUserDB = async (user) => {
-  users.push(user);
-  return user;
+  const { data, error } = await supabaseCli
+    .from("Usuario")
+    .insert([user])
+    .select();
+
+  if (error) {
+    console.error(error);
+    return error;
+  }
+
+  return data;
 };
+
+// const createUserDB = async (user) => {
+//   users.push(user);
+//   return user;
+// };
 
 module.exports = {
   getAllUsers,
